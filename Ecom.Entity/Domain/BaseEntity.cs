7ccd3cb854f3f2ed4d6239e934a594a7025f870d
tasdid <tasdi@LAPTOP-D02SRM5Q>
2023-09-19 18:44:27 +0600

    Add project files.

---
+++ Ecom.Entity/Domain/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Entity.Domain
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public int StatusId { get; set; }
+        
+    }
+}
---
+++ Ecom.Service/productService/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Service.productService
+{
+    public class ProductService : IProductService
+    {
+    }
+}
